2ecc341685a732c54e5531158fa3437c4153fabf
Nikita Lutoshkin <n28280202@gmail.com>
2026-03-17 20:36:38 +0300

    Развёрнут проект

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, loadEnv } from "vite";
+import react from '@vitejs/plugin-react'
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [react()],
+        publicDir: path.resolve(__dirname, "./src/app/public"),
+        server: {
+            cors: false,
+            host: true,
+            port: 3001,
+            strictPort: true,
+            proxy: {
+                "/api": {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                    secure: false,
+                },
+            },
+        },
+    }
+});
